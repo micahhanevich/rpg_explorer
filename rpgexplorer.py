1f5e0649aa54d71bbc25f8225f41efa2ff40f581
mhanevich <mhanevich@divcodata.com>
2022-03-17 17:07:43 -0400

    basic starting formatting

--- rpgexplorer.py
+++ rpgexplorer.py
@@ -2,7 +2,7 @@ import cmd
 import sys
 
 
-def parse_file_path(path: str) -> list[str]:
+def parse_file_path(path: str) -> list:
     return path.split('/')
 
 
@@ -12,7 +12,8 @@ class GameFramework(cmd.Cmd):
 
     def do_cd(self, arg):
         """Change active directories: cd world/continent/city/'potion shop'"""
-        arg = parse_file_path(arg)
+        arg = parse_file_path(arg[0])
+
 
 
 game = GameFramework()
